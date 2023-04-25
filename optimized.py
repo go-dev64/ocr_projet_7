3b420bff6883ad22f3f8ef8c4574f6ad5c1fa754
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-04-25 08:39:00 +0200

    mise a jour

--- optimized.py
+++ optimized.py
@@ -6,21 +6,21 @@ from utile import decorator
 
 
 def clean_csv(data, budget=500):
-    multiplicator = 10
-    data["gain"] = round(data['price'] * data['profit'] / 100, 3)
-    data["price"] = round(data["price"], 2) * multiplicator
+    if data["gain"] is None:
+        data["gain"] = round(data['price'] * data['profit'] / 100, 3)
+        data["price"] = round(data["price"], 2) * 10
     data_cleaned = np.asarray(
         data.loc[(data['price'] > 0) &
-                 (data['price'] < budget * multiplicator) &
+                 (data['price'] < budget * 10) &
                  (data['profit'] > 0), :]
     )
     data_cleaned[:, 1] = np.ceil(data_cleaned[:, 1])
     return data_cleaned
 
 
-data1 = clean_csv(pd.read_csv('csv_file/dataset1.csv'))
-data2 = clean_csv((pd.read_csv('csv_file/dataset2.csv')))
-data20 = clean_csv(pd.read_csv('csv_file/dataset_20.csv'))
+data1 = pd.read_csv('csv_file/dataset1.csv')
+"""data2 = clean_csv((pd.read_csv('csv_file/dataset2.csv')))
+data20 = clean_csv(pd.read_csv('csv_file/dataset_20.csv'))"""
 
 
 def real_coast(actions_list_selected, data):
@@ -39,29 +39,30 @@ def real_coast(actions_list_selected, data):
 
 @decorator
 def dynamique(elements_list, budget=5000):
+    data = clean_csv(elements_list, budget)
     # creation de la matrice vide
-    matrice = np.zeros((len(elements_list) + 1, budget + 1))
+    matrice = np.zeros((len(data) + 1, budget + 1))
     # pour chaque element de la liste (i):
-    for i in np.arange(1, len(elements_list) + 1):
+    for i in np.arange(1, len(data) + 1):
         # pour chaque tranche du budget (W)
         for w in np.arange(1, budget + 1):
             # Si, le cout de l'élément est ≤ à la tranche de prix
-            if elements_list[i - 1][1] <= w:
+            if data[i - 1][1] <= w:
                 # On détermine le profit de l'élément actuel + celle de l'élément au cout précédent.
                 # On garde le max et l'affecte à la position de la matrice pour i et w.
-                matrice[i][w] = max(elements_list[i - 1][3] +
-                                    matrice[i - 1][w - elements_list[i - 1][1]],
+                matrice[i][w] = max(data[i - 1][3] +
+                                    matrice[i - 1][w - data[i - 1][1]],
                                     matrice[i - 1][w])
             else:
                 # Sinon on prend la valeur précédente comme valeur pour la position actuelle
                 matrice[i][w] = matrice[i - 1][w]
 
     w = budget
-    n = len(elements_list)
+    n = len(data)
     element_selection = []
     # on parcourt tous les éléments de la matrice
     while w >= 0 and n >= 0:
-        element = elements_list[n - 1]
+        element = data[n - 1]
         # Si la valeur de la matrice a la position [n][w] =
         # (matrice [n - 1][w - le cout de element ]) + valeur de element
         if matrice[n][w] == matrice[n - 1][w - element[1]] + element[3]:
@@ -73,12 +74,10 @@ def dynamique(elements_list, budget=5000):
         n -= 1
 
     real = real_coast(element_selection, elements_list)
-    new_budget = budget - real[0]
+    new_budget = budget - real[0] * 10
     print(new_budget)
     if new_budget > 0:
         elements_list = np.delete(elements_list, real[2], 0)
-        new_budget = budget - real[0]
-
         print(elements_list, len(elements_list))
         dynamique(elements_list, budget=budget - real[0] * 10)
 
