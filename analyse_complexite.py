f17ff2d94051d94bfe971be61c2084428110282c
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-05-06 12:30:22 +0200

    fix analyse

--- analyse_complexite.py
+++ analyse_complexite.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from bruteforce import list_action_dataset_20, recursivity, iteration
 
-from test import print_result_dynamique, clean_csv
+from test import get_best_invest, clean_csv
 
 from utile import loop
 
@@ -13,10 +13,9 @@ import math
 
 data_20_clean = clean_csv(pd.read_csv('csv_file/dataset_24.csv'))
 data1 = clean_csv(pd.read_csv('csv_file/dataset1.csv'))
-array_dynamique = loop(data_20_clean, print_result_dynamique,  24, 1)
+array_dynamique = loop(data_20_clean, get_best_invest,  24, 1)
 array_recursive = loop(list_action_dataset_20, recursivity, 24, 1)
 array_binary = loop(list_action_dataset_20, iteration, 24, 1)
-
 
 
 plt.plot(array_recursive[2], array_recursive[0], label='bruteForce récursif', color='blue')
